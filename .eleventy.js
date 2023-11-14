bca30a9a90ae3d7c795afaec50c3d73571bc9d57
Rob McLarty <r@robmclarty.com>
2023-11-13 16:01:09 -0800

    fix: pass through /about/cv

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,8 @@ module.exports = eleventyConfig => {
   // Manually copy static assets.
   eleventyConfig.addPassthroughCopy('src/stylesheets')
   eleventyConfig.addPassthroughCopy('src/images')
-  eleventyConfig.addPassthroughCopy("src/javascripts");
+  eleventyConfig.addPassthroughCopy("src/javascripts")
+  eleventyConfig.addPassthroughCopy('src/about/*')
   eleventyConfig.addPassthroughCopy('src/favicon.ico')
 
   // Exclude from tagList.
